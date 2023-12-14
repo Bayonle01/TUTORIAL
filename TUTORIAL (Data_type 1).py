e0a6129159577b17538c8c5e98459c581d93e843
Bayonle01 <mudashirawwal2022@gmail.com>
2023-12-14 04:39:29 -0800

    Tutorials

---
+++ TUTORIAL (Data_type 1).py
@@ -0,0 +1,125 @@
+users = ["Dave","John","Sara"]
+
+data = ["Dave", 42, True]
+
+emptylist = []
+print("Dave" in data) 
+print("Dave" in users)
+print("Dave" in emptylist)
+
+print(users[0])
+print(users[-2])
+
+print(users.index("Sara"))            
+
+print(users[0:2])
+print(users[1:])
+print(users[-3:-1])
+
+print(len(data))
+
+
+users.append("Elsa")
+print(users)
+
+users = users + ["Jason"]
+print(len(users), users)
+
+users.extend(["Robert", "Jimmy", "Ade"])
+print(users)
+
+#users.extend(data)
+print(users) 
+
+users.insert(0, "Bob")
+print(users)
+
+users[4:6] = ["Eddie", "Alex"]
+print(users)
+
+users[1:3] = ["Robert", "JPJ"]
+print(users)
+
+users.remove("Eddie")
+print(users)
+
+users.pop()
+print(users) 
+ 
+del users[1]
+print(users)
+
+
+#del data
+#print(data)
+
+
+data.clear()
+print(data)
+
+users[1:2] = ["dave"]
+users.sort()
+print(users)
+print('')
+print('')
+print('')
+print('')
+
+users.sort(key=str.lower)
+print(users) 
+
+nums = [4, 42, 78, 1, 5]
+nums.reverse()
+print(nums)
+
+#nums.sort(reverse=True)
+#print(nums)
+
+print(sorted(nums,reverse=True))
+print(nums)
+
+numscopy = nums.copy()
+mynums = list(nums)
+mycopy = nums[:]
+print(" ")
+
+print(numscopy)
+print(mynums)
+mycopy.sort()
+print(mycopy)
+print(nums)
+
+print(type(nums))
+
+mylist = list([1,"Neil",True])
+print(mylist)
+
+#Tuples
+mytuple = tuple(('Dave',42,True))
+anothertuple = (1,4,2,8,4)
+
+print(mytuple)
+print(type(mytuple))
+print(type(anothertuple))
+
+newlist = list(mytuple)
+print(type(newlist))
+newlist.append("Neil")
+print(newlist)
+newtuple = tuple(newlist)
+print(newtuple)
+print(newlist[1])
+print(newtuple[0])
+
+(ow, *re, gf) = anothertuple   #NOTE: it can work for list also 
+print(ow, re)
+print(gf)
+
+print(anothertuple.count(4))
+
+
+
+ 
+
+
+
---
+++ TUTORIAL (Dict & Set).py
@@ -0,0 +1,149 @@
+# Dictionaries
+band = {
+    "vocals": "Plant",
+    "guitar": "Page"
+    }
+
+band2 = dict(vocal= " plant", guitar= "Page")
+print(band)
+print(type(band))
+print(len(band2))
+print(band2)
+#Access items on dictionary
+print(band["vocals"])
+print(band.get("guitar"))
+
+#list all keys and values
+print(band.keys())
+print(band.values())
+
+#list of key/values pairs as tuples
+print(band.items())
+
+#Verify a key exists
+print("guitar" in band)
+print("angular" in band)
+
+#change values
+band["vocals"] = "Coverdale"
+print(band)
+band.update({"bass":"JPJ"})
+print(band)
+
+#Remove items
+band.pop("bass")  
+print(band)
+
+band["drums"] = "Bonham"
+print(band)
+print(band.popitem()) #tuple
+print(band)
+
+#Delete and clear
+band["drums"] = "Bonham"
+print(band)
+del band["guitar"]
+print(band)
+
+band2.clear()
+print(band2)
+
+del band2
+
+'''
+#Copy dictionaries
+band2 = band #create a reference
+print("Bad copy!")
+print(band2)
+print(band)
+
+
+band2["drum"]="Dave"
+print(band)
+'''
+band2 = band.copy()
+band2["drum"] = "Dave"
+print("Good copy!")
+print(band)
+print(band2)
+
+# Or use the dict() constructor function
+band3 = dict(band)
+print("Good Copy!")
+print(band3)
+
+member1 = {
+    "name" : "Plant",
+    "Instrument" : "vocals"
+}
+
+member2 = {
+    "name" : "Page",
+    "Instrument" : "guitar"
+}
+
+band = {
+    "member1" : member1,
+    "member2" : member2
+    }
+print(band)
+print(band.get("member1"))
+print(band['member1']['name'])
+print("")
+
+
+#SETS
+
+nums = {1,2,3,4}
+nums2 = set((1,2,3,4))
+print(nums)
+print(nums2)
+print(type(nums2))
+print(len(nums2))
+
+# No duplicate allowed
+nums = {1,2,2,3}
+print(nums)
+
+# True is a dupe of 1, False is a dupe of zero
+nums = {1, True, 2, False, 3, 4, 0}
+print(nums)
+
+# To check if a value is in a set
+print(2 in nums)
+
+# Buh you cannot refer to an element in the set with an index
+# position or a key
+   
+# Add a new element to a set
+nums.add(8)
+print(nums)
+
+# Add elements from one set to another
+morenums = {5, 6, 7}
+nums.update(morenums)
+print(nums)
+
+# NOTE: You can use update with lists, tuples, and dictionaries too.
+
+# Merge two sets to create a new set
+one = {1,2,3}
+two = {5,6,7}
+
+mynewset = one.union(two)
+print(mynewset)
+
+# Keep only the duplicate
+one = {1,2,3}
+two = {2,3,4}
+one.intersection_update(two)
+print(one)
+
+# Keep everything except the duplicate
+one = {1,2,3}
+two = {2,3,4}
+one.symmetric_difference_update(two)
+print(one)
+
+
+
